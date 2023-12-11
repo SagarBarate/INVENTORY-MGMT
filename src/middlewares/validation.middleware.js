95441ed9eba15ab61a0477d209bba0467c51c382
Sagar Barate <sagarbarate2907@com.com>
2023-12-11 07:28:54 +0530

    express validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,33 +1,46 @@
-//hostedDecalration => a funtion
-//class
-//assignment declaration
-
-import { validationResult } from "express-validator";
-
-
-const validateRequest = async(req,res,next) =>{
-     // validate data
-    //1. Setup rules for validation.
+import {
+    body,
+    validationResult,
+  } from 'express-validator';
+  
+  const validateRequest = async (
+    req,
+    res,
+    next
+  ) => {
+    console.log(req.body);
+    // 1. Setup rules for validation.
     const rules = [
-        body('name').isEmpty().withMessage('Name is required'),
-        body('price').isFloat({gt:0}).withMessage('Price should be a positive value'),
-        body('imageUrl').isUrl().withMessage('Invalid url'),
+      body('name')
+        .notEmpty()
+        .withMessage('Name is required'),
+      body('price')
+        .isFloat({ gt: 0 })
+        .withMessage(
+          'Price should be a positive value'
+        ),
+      body('imageUrl')
+        .isURL()
+        .withMessage('Invalid url'),
     ];
-
-    //2. run those rules
-    await Promise.all(rules.map(rule=> rule.run(req)))
-
-    //3.check if there are any errors after running the rules.
-    var errors = validationResult(req);
-
-    //4. if errors , return the error message
-    
-     if (errors.length > 0) {
-       return res.render('new-product', {
-         errorMessage: errors[0],
-       });
-     }
-     next();
-};
-
-export default validateRequest;
+  
+    // 2. run those rules.
+    await Promise.all(
+      rules.map((rule) => rule.run(req))
+    );
+  
+    // 3. check if there are any errors after running the rules.
+    var validationErrors = validationResult(req);
+    console.log(validationErrors);
+    // 4. if errros, return the error message
+    if (!validationErrors.isEmpty()) {
+      return res.render('new-product', {
+        errorMessage:
+          validationErrors.array()[0].msg,
+      });
+    }
+    next();
+  };
+  
+  export default validateRequest;
+  
